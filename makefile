592aa85c22cd99bc2e98ed9673cbc8549bebc890
Narender Nain <narenderkumarnain@gmail.com>
2024-09-02 14:55:32 +0530

    basics

---
+++ makefile
@@ -0,0 +1,5 @@
+# FILE ?= default.cpp
+
+run: $(FILE)
+	g++-14 -std=c++17 -o ./a.out $(FILE)
+	./a.out < input.txt > output.txt
---
+++ template.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* template under progress */
+
+/* template end here */
+int main() {
+    
+    return 0;
+}
